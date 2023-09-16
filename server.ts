b0ea18b977b225ab1769a38bfbe7072a5eafdec3
Flavio Andrade <foa12381@gmail.com>
2023-09-16 14:13:29 -0300

    add multerpart

--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
 import { fastify } from 'fastify';
+import { getAllPromptsRoute } from './src/routes/routePrompts';
+import { uploadVideoRoute } from './src/routes/routeUploadVideo';
 
 const port = process.env.PORT || 3333
 
 const app = fastify()
 
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 
-app.get("/", ()=> "hello word")
 
 app.listen(
   {
