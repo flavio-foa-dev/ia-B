6e787749f32f0216ec4c4dd227c0946d108399bf
Flavio Andrade <foa12381@gmail.com>
2023-09-20 15:13:14 -0300

    add ia routs.http

--- src/routes/routeGenerateCompletion.ts
+++ src/routes/routeGenerateCompletion.ts
@@ -9,11 +9,11 @@ export async function genereteCompleteRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where:{
@@ -25,9 +25,9 @@ export async function genereteCompleteRoute(app: FastifyInstance) {
       return reply.status(400).send({ error: 'No video transcription was not generate yet'})
     }
 
-    const promptMessage = template.replace("{transcription}", video.transcript)
+    const promptMessage = prompt.replace("{transcription}", video.transcript)
     const response = openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k",
       temperature,
       messages: [
         {
@@ -36,10 +36,6 @@ export async function genereteCompleteRoute(app: FastifyInstance) {
         },
       ],
     })
-
-
-
-
     return response
   })
 
